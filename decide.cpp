ea6bd44494216b5641dd06bbe1120fe2267dbefa
VonKavalier <celestin.rocker.tom15@gmail.com>
2016-09-04 21:14:56 +0200

    mise en ligne du fichier source et de l'executable

---
+++ decide.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    srand (time(NULL));
+    int choix;
+
+    cout << "Entre combien de choses hésites-tu ? : ";
+
+    cin >> choix;
+
+    cin.ignore();
+
+    string chose;
+
+    string choses[choix];
+
+    for(int i=0; i<choix; i++){
+        cout << "Rentre la chose " << i+1 << ": ";
+        getline(cin, chose);
+        choses[i]=chose;
+    }
+
+    cout << "Décision : " << choses[rand() % choix+1] << "\n";
+
+    cin.get();
+
+    return 0;
+
+}
